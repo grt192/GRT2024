003c5f18e86f6238e99e6343f933dafc2d9e8476
Pointy Ice <jessecsh7@gmail.com>
2024-01-18 17:40:01 -0800

    Elevator commands init

---
+++ src/main/java/frc/robot/commands/ElevatorToGroundCommand.java
@@ -0,0 +1,31 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator.ElevatorSubsystem;
+import frc.robot.subsystems.Elevator.ElevatorState;
+
+public class ElevatorToGroundCommand extends Command{
+    private ElevatorSubsystem elevatorSubsystem;
+    public ElevatorToGroundCommand(ElevatorSubsystem elevatorSubsystem){
+        this.addRequirements(elevatorSubsystem);
+        this.elevatorSubsystem = elevatorSubsystem;
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        return;
+    }
+
+    @Override
+    public void execute(){
+        this.elevatorSubsystem.setTargetState(ElevatorState.GROUND);
+    }
+
+    @Override
+    public boolean isFinished(){
+        if(this.elevatorSubsystem.getState()==ElevatorState.GROUND){
+            return true;
+        }
+        else return false;
+    }
+}
