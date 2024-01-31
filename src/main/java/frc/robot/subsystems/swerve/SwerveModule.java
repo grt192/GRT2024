fb06ca889c088ad5bd1627da950c14c20e556d5c
penguin212 <dhuber06@gmail.com>
2024-01-30 20:27:38 -0800

    vortex changes

--- src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
@@ -81,7 +81,7 @@ public class SwerveModule {
         // untested for vortexes
         driveMotor.setPositionConversionFactor(DRIVE_ROTATIONS_PER_METER);
         System.out.println("factor " + DRIVE_ROTATIONS_PER_METER);
-        driveMotor.setVelocityConversionFactor(DRIVE_ROTATIONS_PER_METER * 60.0); //Conversion from rpm to m/s
+        driveMotor.setVelocityConversionFactor(DRIVE_ROTATIONS_PER_METER / 60.0); //Conversion from rpm to m/s
         
         steerMotor = new CANSparkMax(steerPort, MotorType.kBrushless);
         // steerMotor.setInverted(true);
