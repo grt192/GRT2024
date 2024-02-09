fca98a134b1c8a992c55f39378d76a09dd9b3227
penguin212 <dhuber06@gmail.com>
2024-02-09 00:25:23 -0800

    run intake for a little longer to make sure note goes in

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -144,7 +144,8 @@ public class RobotContainer {
 
   private void configureBindings() {
         aButton.onTrue(new ElevatorToChuteCommand(elevatorSubsystem).andThen(
-                       new IntakeRollerIntakeCommand(intakeRollerSubsystem)));
+                       new IntakeRollerIntakeCommand(intakeRollerSubsystem).andThen(
+                       new IntakeRollerFeedCommand(intakeRollerSubsystem).withTimeout(.1))));
 
         bButton.onTrue(new IdleCommand(intakePivotSubsystem, intakeRollerSubsystem, 
                                        elevatorSubsystem, 
