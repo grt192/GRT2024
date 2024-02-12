7a57ca6e4b7640b1f70688151b27682022350cb1
Rishay Jain <rishayjain@gmail.com>
2024-02-12 00:37:14 -0800

    pathfinding set up

--- src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
@@ -163,9 +163,9 @@ public class SwerveSubsystem extends BaseSwerveSubsystem{
             new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
                         new PIDConstants(3.0, 0.0, 0.0), // Translation PID constants
                         new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
-                        2, // Max module speed, in m/s
-                        Constants.SwerveConstants.MODULE_DIST * 1.414, // Drive base radius in meters. Distance from robot center to furthest module.
-                        new ReplanningConfig() // Default path replanning config. See the API for the options here
+                        4.5, // Max module speed, in m/s
+                        FL_POS.getNorm(), // Drive base radius in meters. Distance from robot center to furthest module.
+                        new ReplanningConfig(true, true)
                 ),
             () -> {
                 // Boolean supplier that controls when the path will be mirrored for the red alliance
