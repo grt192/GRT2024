2442265d7be8e2621336637269de08433746651f
penguin212 <dhuber06@gmail.com>
2024-02-19 20:41:09 -0800

    init?!

--- src/main/java/frc/robot/commands/auton/BaseAutonSequence.java
+++ src/main/java/frc/robot/commands/auton/BaseAutonSequence.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.auton;
- 
+
+import com.choreo.lib.Choreo;
+import com.choreo.lib.ChoreoTrajectory;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -21,16 +23,15 @@ import frc.robot.subsystems.shooter.ShooterFlywheelSubsystem;
 import frc.robot.subsystems.shooter.ShooterPivotSubsystem;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
 
-
-import com.choreo.lib.Choreo;
-import com.choreo.lib.ChoreoTrajectory;
-
-
-public class BaseAutonSequence extends SequentialCommandGroup{
+/** 
+ * The base autonomous sequence that other autons extend. This class provides functions that abstract shared tasks 
+ * between autons
+ */
+public class BaseAutonSequence extends SequentialCommandGroup {
 
     private final IntakePivotSubsystem intakePivotSubsystem;
     private final IntakeRollersSubsystem intakeRollersSubsystem;
-    private final ShooterFeederSubsystem shooterFeederSubsystem; 
+    private final ShooterFeederSubsystem shooterFeederSubsystem;
     private final ShooterFlywheelSubsystem shooterFlywheelSubsystem;
     private final ShooterPivotSubsystem shooterPivotSubsystem;
     private final ElevatorSubsystem elevatorSubsystem;
@@ -41,13 +42,14 @@ public class BaseAutonSequence extends SequentialCommandGroup{
     private PIDController yPID;
     private boolean isRed;
     private int driveforwardtime;
-
-    public BaseAutonSequence(IntakePivotSubsystem intakePivotSubsystem, IntakeRollersSubsystem intakeRollersSubsystem, 
-                             ShooterFeederSubsystem shooterFeederSubsystem, ShooterFlywheelSubsystem shooterFlywheelSubsystem, ShooterPivotSubsystem shooterPivotSubsystem, 
-                             ElevatorSubsystem elevatorSubsystem, 
-                             SwerveSubsystem swerveSubsystem,
-                             LEDSubsystem ledSubsystem){
-        this.intakePivotSubsystem = intakePivotSubsystem; 
+    
+    public BaseAutonSequence(IntakePivotSubsystem intakePivotSubsystem, IntakeRollersSubsystem intakeRollersSubsystem,
+            ShooterFeederSubsystem shooterFeederSubsystem, ShooterFlywheelSubsystem shooterFlywheelSubsystem,
+            ShooterPivotSubsystem shooterPivotSubsystem,
+            ElevatorSubsystem elevatorSubsystem,
+            SwerveSubsystem swerveSubsystem,
+            LEDSubsystem ledSubsystem) {
+        this.intakePivotSubsystem = intakePivotSubsystem;
         this.intakeRollersSubsystem = intakeRollersSubsystem;
         this.shooterFeederSubsystem = shooterFeederSubsystem;
         this.shooterFlywheelSubsystem = shooterFlywheelSubsystem;
@@ -56,50 +58,65 @@ public class BaseAutonSequence extends SequentialCommandGroup{
         this.swerveSubsystem = swerveSubsystem;
         this.ledSubsystem = ledSubsystem;
 
-        addRequirements(swerveSubsystem, intakeRollersSubsystem,intakePivotSubsystem);
+        addRequirements(swerveSubsystem, intakeRollersSubsystem, intakePivotSubsystem);
 
         isRed = DriverStation.getAlliance().get() == DriverStation.Alliance.Red;
-    
+
         swerveSubsystem = new SwerveSubsystem();
 
         xPID = new PIDController(4, 0, 0);
         yPID = new PIDController(12, 0, 0);
         thetaController = new PIDController(3, 0, 0);
     }
-
-    public Command followPath(ChoreoTrajectory traj){
+    
+    
+    
+    public Command followPath(ChoreoTrajectory traj) {
         Command swerveCommand = Choreo.choreoSwerveCommand(
-            traj,
-            swerveSubsystem::getRobotPosition,
-            xPID, 
-            yPID, 
-            thetaController, 
-            ((ChassisSpeeds speeds) -> {swerveSubsystem.setChassisSpeeds(
-                speeds.vxMetersPerSecond,
-                speeds.vyMetersPerSecond, 
-                speeds.omegaRadiansPerSecond
-                );
-            System.out.println(speeds.vxMetersPerSecond);}),
-            () -> isRed,
-            swerveSubsystem
-            );
+                traj,
+                swerveSubsystem::getRobotPosition,
+                xPID,
+                yPID,
+                thetaController,
+                ((ChassisSpeeds speeds) -> {
+                    swerveSubsystem.setChassisSpeeds(
+                            speeds.vxMetersPerSecond,
+                            speeds.vyMetersPerSecond,
+                            speeds.omegaRadiansPerSecond);
+                    System.out.println(speeds.vxMetersPerSecond);
+                }),
+                () -> isRed,
+                swerveSubsystem);
         return swerveCommand;
     }
 
-    public SequentialCommandGroup goIntake(ChoreoTrajectory intaketraj){
+    
+    /** 
+     * @param intaketraj
+     * @return SequentialCommandGroup
+     */
+    public SequentialCommandGroup goIntake(ChoreoTrajectory intaketraj) {
         return followPath(intaketraj)
-        .andThen(new IntakePivotExtendedCommand(intakePivotSubsystem))
-        .andThen(new IntakeRollerIntakeCommand(intakeRollersSubsystem, ledSubsystem).raceWith(new DriveForwardCommand(swerveSubsystem).withTimeout(driveforwardtime)))
-        .andThen(new IntakeRollerIntakeCommand(intakeRollersSubsystem, ledSubsystem))//just in case the note isn't fully intaken above
-        .andThen(new IntakeRollerFeedCommand(intakeRollersSubsystem))
-        .andThen(new IntakePivotVerticalCommand(intakePivotSubsystem));
+                .andThen(new IntakePivotExtendedCommand(intakePivotSubsystem))
+                .andThen(new IntakeRollerIntakeCommand(intakeRollersSubsystem, ledSubsystem)
+                        .raceWith(new DriveForwardCommand(swerveSubsystem).withTimeout(driveforwardtime)))
+                .andThen(new IntakeRollerIntakeCommand(intakeRollersSubsystem, ledSubsystem))// just in case the note
+                                                                                             // isn't fully intaken
+                                                                                             // above
+                .andThen(new IntakeRollerFeedCommand(intakeRollersSubsystem))
+                .andThen(new IntakePivotVerticalCommand(intakePivotSubsystem));
     }
 
-    public SequentialCommandGroup goShoot(ChoreoTrajectory shoottraj){
+    /** 
+     * @param shoottraj
+     * @return SequentialCommandGroup
+     */
+    public SequentialCommandGroup goShoot(ChoreoTrajectory shoottraj) {
         return followPath(shoottraj)
-        .andThen(new ShootModeSequence(intakeRollersSubsystem, elevatorSubsystem, shooterFeederSubsystem, shooterFlywheelSubsystem, shooterPivotSubsystem, ledSubsystem))
-        .andThen(new ShooterFeedShootCommand(shooterFeederSubsystem))
-        .andThen(new ShooterFlywheelStopCommand(shooterFlywheelSubsystem));
+                .andThen(new ShootModeSequence(intakeRollersSubsystem, elevatorSubsystem, shooterFeederSubsystem,
+                        shooterFlywheelSubsystem, shooterPivotSubsystem, ledSubsystem))
+                .andThen(new ShooterFeedShootCommand(shooterFeederSubsystem))
+                .andThen(new ShooterFlywheelStopCommand(shooterFlywheelSubsystem));
     }
 
 }
