08be89db5f41a98104b77ebb86ed23e7845f8dda
CrolineCrois <maoninii@gmail.com>
2024-03-01 09:07:30 -0800

    cruaton with red-blue sides

--- src/main/java/frc/robot/commands/auton/TopPreloadedSequence.java
+++ src/main/java/frc/robot/commands/auton/TopPreloadedSequence.java
@@ -2,7 +2,9 @@ package frc.robot.commands.auton;
 
 import com.choreo.lib.ChoreoTrajectory;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.Constants.SwerveConstants;
 import frc.robot.commands.shooter.pivot.ShooterPivotAimCommand;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
@@ -15,16 +17,19 @@ import frc.robot.subsystems.swerve.SwerveSubsystem;
 
 public class TopPreloadedSequence extends BaseAutonSequence{
 
-    private double targetRads = SwerveConstants.IS_RED ? Math.PI - 2.18 : -2.18;
-    private Rotation2d preloadedShootAngle = new Rotation2d(targetRads);
+    // private double targetRads = SwerveConstants.IS_RED ? Math.PI - 2.18 : -2.18;
+    // private Rotation2d preloadedShootAngle = new Rotation2d(targetRads);
+
+        private Pose2d initPose = SwerveConstants.IS_RED ? new Pose2d(new Translation2d(15.11, 7.01), new Rotation2d(Math.PI)) : new Pose2d(new Translation2d(1.43, 7.01), new Rotation2d(0));
    
     public TopPreloadedSequence(IntakePivotSubsystem intakePivotSubsystem, IntakeRollersSubsystem intakeRollersSubsystem, 
                                 ShooterFlywheelSubsystem shooterFlywheelSubsystem, ShooterPivotSubsystem shooterPivotSubsystem, 
                                ElevatorSubsystem elevatorSubsystem, SwerveSubsystem swerveSubsystem, LEDSubsystem ledSubsystem) {
         super(intakePivotSubsystem, intakeRollersSubsystem, shooterFlywheelSubsystem, shooterPivotSubsystem, elevatorSubsystem, swerveSubsystem, ledSubsystem);
+        ((SwerveSubsystem) swerveSubsystem).resetPose(initPose);
 
         addCommands(
-            new SetHardAngleCommand(swerveSubsystem, preloadedShootAngle),
+            new SetCalculatedAngleCommand(swerveSubsystem),
             shoot()
         );
     }
