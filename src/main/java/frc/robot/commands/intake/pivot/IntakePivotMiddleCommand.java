0181648f6d5b6cba75b132f61c7d3657f2569767
penguin212 <dhuber06@gmail.com>
2024-03-10 00:00:00 -0800

    oop

--- src/main/java/frc/robot/commands/intake/pivot/IntakePivotMiddleCommand.java
+++ src/main/java/frc/robot/commands/intake/pivot/IntakePivotMiddleCommand.java
@@ -28,7 +28,7 @@ public class IntakePivotMiddleCommand  extends Command{
     }
     @Override
     public void execute() {
-        System.out.println("PIVOT MOVING" + pivotSubsystem.getPosition());
+        System.out.println("PIVOT MOVING" + pivotSubsystem.getEncoderPosition());
         //TODO Auto-generated method stub
     }
 
@@ -45,7 +45,7 @@ public class IntakePivotMiddleCommand  extends Command{
 
     @Override
     public boolean isFinished() {
-        return Math.abs(pivotSubsystem.getPosition() - position) < .1;
+        return Math.abs(pivotSubsystem.getEncoderPosition() - position) < .1;
         // TODO Auto-generated method stub
        
     }
