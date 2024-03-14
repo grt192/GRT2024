8446eb23e849627ef90364f42fbd91fd3ad39982
Pointy Ice <jessecsh7@gmail.com>
2024-03-14 14:21:07 -0700

    Removed networktable features.

--- src/main/java/frc/robot/commands/elevator/ElevatorToZeroCommand.java
+++ src/main/java/frc/robot/commands/elevator/ElevatorToZeroCommand.java
@@ -1,8 +1,5 @@
 package frc.robot.commands.elevator;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.elevator.ElevatorState;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
@@ -10,9 +7,6 @@ import frc.robot.subsystems.elevator.ElevatorSubsystem;
 /** Sends the elevator to position zero. */
 public class ElevatorToZeroCommand extends Command {
     private ElevatorSubsystem elevatorSubsystem;
-    private NetworkTableInstance nt;
-    private NetworkTable table;
-    private NetworkTableEntry entry;
     
     /** Constructs a {@link ElevatorToZeroCommand} for the specified elevator. */
     public ElevatorToZeroCommand(ElevatorSubsystem elevatorSubsystem) {
@@ -22,10 +16,6 @@ public class ElevatorToZeroCommand extends Command {
 
     @Override
     public void end(boolean interrupted) {
-        nt = NetworkTableInstance.getDefault();
-        table = nt.getTable("elevator");
-        entry = table.getEntry("position");
-        entry.setInteger(0);
     }
     
     @Override
