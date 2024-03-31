af19e7ffe28ad58aa31fcad4227f5eeb33328830
Jesse Chu <jesse.chu@outlook.com>
2024-03-30 22:38:19 -0700

    Removed unused imports

--- src/main/java/frc/robot/commands/elevator/ElevatorToLimitSwitchCommand.java
+++ src/main/java/frc/robot/commands/elevator/ElevatorToLimitSwitchCommand.java
@@ -2,7 +2,6 @@ package frc.robot.commands.elevator;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
-import frc.robot.Constants.ElevatorConstants;
 
 /** Sends the elevator to position zero. */
 public class ElevatorToLimitSwitchCommand extends Command {
